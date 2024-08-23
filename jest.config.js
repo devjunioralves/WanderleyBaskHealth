30efea886b8b0ed626435a4d97bf59b5922249d5
Junior Trindade <jrwanderley17@gmail.com>
2024-08-23 08:11:50 -0300

    chore: fix jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
-  moduleNameMapper: {
-    '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1',
-  },
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  coverageDirectory: '<rootDir>/../coverage',
+  testEnvironment: 'node',
+  rootDir: 'src',
   transform: {
-    '\\.ts$': 'ts-jest',
+    '.+\\.ts$': 'ts-jest',
   },
-  preset: '@shelf/jest-mongodb',
+  setupFiles: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '^@application/(.*)$': '<rootDir>/application/$1',
+    '^@config/(.*)$': '<rootDir>/config/$1',
+    '^@di/(.*)$': '<rootDir>/di/$1',
+    '^@domain/(.*)$': '<rootDir>/domain/$1',
+    '^@infra/(.*)$': '<rootDir>/infra/$1',
+    '^@logger/(.*)$': '<rootDir>/logger/$1',
+    '^@presentation/(.*)$': '<rootDir>/presentation/$1',
+    '^@shared/(.*)$': '<rootDir>/shared/$1',
+    '^@src/(.*)$': '<rootDir>/$1',
+  },
 }
